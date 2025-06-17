c433367001b0699ca26b5920f0481f17fee9cae7
moonseopage1 <moon@seopage1.net>
2025-06-17 17:06:06 +0600

    :recycle: Quil editor modify

--- src/components/QuilFromItem.jsx
+++ src/components/QuilFromItem.jsx
@@ -1,6 +1,7 @@
+// TODO: need to fix required validation
 import { Form } from "antd";
-import ReactQuill from "react-quill-new";
-import "react-quill-new/dist/quill.snow.css";
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 import PropTypes from "prop-types";
 
 const isQuillEmpty = (html) => {
@@ -12,7 +13,7 @@ const isQuillEmpty = (html) => {
 const QuillFormItem = ({
     name,
     label,
-    requiredMessage,
+    requiredMessage = "This field is required",
     ...rest
 }) => {
     return (
@@ -20,17 +21,15 @@ const QuillFormItem = ({
             name={name}
             label={<span className="text-lg">{label}</span>}
             valuePropName="value"
-            getValueFromEvent={(content) => content} // required for ReactQuill to update Form
             rules={[
                 {
-                    required: requiredMessage ? true : false,
+                    required: true,
                     validator: (_, value) =>
                         isQuillEmpty(value)
                             ? Promise.reject(new Error(requiredMessage))
                             : Promise.resolve(),
                 },
             ]}
-            validateTrigger="onChange"
             labelCol={{ span: 24 }}
             wrapperCol={{ span: 24 }}
         >
@@ -40,68 +39,37 @@ const QuillFormItem = ({
                 {...rest}
             />
         </Form.Item>
+        // <Form.Item
+        //     name={name}
+        //     label={<span className="text-lg">{label}</span>}
+        //     rules={[
+        //         {
+        //             validator: (_, value) =>
+        //                 isQuillEmpty(value)
+        //                     ? Promise.reject(new Error(requiredMessage))
+        //                     : Promise.resolve(),
+        //         },
+        //     ]}
+        //     valuePropName="value"
+        //     trigger="onChange"
+        //     validateTrigger="onChange"
+        //     labelCol={{ span: 24 }}
+        //     wrapperCol={{ span: 24 }}
+        // >
+        //     <ReactQuill
+        //         theme="snow"
+        //         placeholder={`Write your ${label?.toLowerCase()}`}
+        //         {...rest}
+        //     />
+        // </Form.Item>
+
     );
 };
 
-
-// const QuillFormItem = ({
-//     name,
-//     label,
-//     requiredMessage = "This field is required",
-//     ...rest
-// }) => {
-//     // eslint-disable-next-line no-unused-vars
-//     const [editorValue, setEditorValue] = useState("");
-
-//     // Clear the editor when form is reset
-//     const handleChange = (value) => {
-//         setEditorValue(value);
-//     };
-
-//     return (
-//         <Form.Item
-//             shouldUpdate={(prevValues, currentValues) =>
-//                 prevValues[name] !== currentValues[name]
-//             }
-//             noStyle
-//         >
-//             {({ getFieldValue }) => (
-//                 <Form.Item
-//                     name={name}
-//                     label={<span className="text-lg">{label}</span>}
-//                     valuePropName="value"
-//                     trigger="onChange"
-//                     validateTrigger="onChange"
-//                     rules={[
-
-//                         {
-//                             required: requiredMessage ? true : false,
-//                             validator: (_, value) =>
-//                                 isQuillEmpty(value)
-//                                     ? Promise.reject(new Error(requiredMessage))
-//                                     : Promise.resolve(),
-//                         },
-//                     ]}
-//                     labelCol={{ span: 24 }}
-//                     wrapperCol={{ span: 24 }}
-//                 >
-//                     <ReactQuill
-//                         theme="snow"
-//                         value={getFieldValue(name) || ""}
-//                         onChange={handleChange}
-//                         placeholder={`Write your ${label?.toLowerCase()}`}
-//                         {...rest}
-//                     />
-//                 </Form.Item>
-//             )}
-//         </Form.Item>
-//     );
-// };
+export default QuillFormItem;
 
 QuillFormItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    label: PropTypes.string,
     requiredMessage: PropTypes.string,
 };
-
-export default QuillFormItem;
